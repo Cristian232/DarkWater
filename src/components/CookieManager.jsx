c83adeef9faa3e40ccd104c515a000e52d8cab9c
Cristian <hyperc232@gmail.com>
2024-05-12 21:58:13 +0300

    updateDomain bug fix 3 change to cookie manager

--- src/components/CookieManager.jsx
+++ src/components/CookieManager.jsx
@@ -2,19 +2,15 @@ import Cookie from 'js-cookie';
 
 const CookieManager = {
     setSessionCookie: (token) => {
-        // const timestamp = Math.floor(Date.now() / 1000); // Current time in seconds
-        // const base64Token = btoa(String(timestamp)); // Convert timestamp to string and encode in Base64
-        // Cookie.set('session_id', base64Token, { expires: 1 }); // Set cookie with the encoded token
-        Cookie.set('session_id', token, { expires: 1 });
-
+        Cookie.set('DNSWebSession', token, { expires: 1 });
     },
 
     getSessionCookie: () => {
-        return Cookie.get('session_id');
+        return Cookie.get('DNSWebSession');
     },
 
     removeSessionCookie: () => {
-        Cookie.remove('session_id');
+        Cookie.remove('DNSWebSession');
     }
 };
 
