2804e79aa22e624bedb402d5e80a5330014e322c
Cristian <hyperc232@gmail.com>
2024-05-14 17:25:01 +0300

    add get_logs part 1 /// bug fix fetch //part8 6

--- src/components/Logs.jsx
+++ src/components/Logs.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react';
 import axios from '../api/AxiosConfig.jsx';
-import { useNavigate } from 'react-router-dom';
+import styles from "./Dashboard.module.css";
+
 
 
 
 function Logs() {
     const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
 
-    const navigate = useNavigate();
+
 
     const handleGuestLogin = async () => {
         // Logic for guest access
@@ -20,10 +18,10 @@ function Logs() {
             console.log("TEST logs" +
                 response.data
             )
+            setUsername(response.data)
             // navigate('/dashboardUnauth');
         } catch (axiosError) {
             console.error("Login failed", axiosError);
-            setError('Failed to login. Please check your credentials and try again.');
         }
     };
 
@@ -34,6 +32,16 @@ function Logs() {
                 onClick={() => handleGuestLogin()}
                 className={'styles.actionButton'}>Check Logs
             </button>
+            <ul className={styles.domainsList}>
+                {/*{domains.map((domain, index) => index === 0 ? (*/}
+                {/*    // Make the first domain's name editable*/}
+                {/*    <li key={domain.id || index}>*/}
+                {/*       */}
+                {/*    </li>*/}
+                {/*) */}
+                {/*))}*/}
+                <p>test </p>
+            </ul>
         </div>
     );
 }
